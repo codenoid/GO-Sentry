85f6b8627a9c3d1d6217edd65d854ec538cecf2b
Elixir & Go ‏‏‎ <jihantoro8@gmail.com>
2018-12-06 13:54:53 +0700

    Update sentry_cli.cr

--- src/sentry_cli.cr
+++ src/sentry_cli.cr
@@ -3,21 +3,18 @@ require "colorize"
 require "./sentry"
 
 begin
-  shard_yml = YAML.parse File.read("shard.yml")
-  name = shard_yml["name"]?
-  Sentry::Config.shard_name = name.as_s if name
+  Sentry::Config.name = File.basename(__DIR__)
 rescue e
 end
 
 cli_config = Sentry::Config.new
-cli_config_file_name = ".sentry.yml"
 
 OptionParser.parse! do |parser|
   parser.banner = "Usage: ./sentry [options]"
   parser.on(
     "-n NAME",
     "--name=NAME",
-    "Sets the display name of the app process (default name: #{Sentry::Config.shard_name})") { |name| cli_config.display_name = name }
+    "Sets the display name of the app process (default name: #{Sentry::Config.name})") { |name| cli_config.display_name = name }
   parser.on(
     "-b COMMAND",
     "--build=COMMAND",
@@ -42,17 +39,6 @@ OptionParser.parse! do |parser|
     cli_config.watch << file
   end
   parser.on(
-    "-c FILE",
-    "--config=FILE",
-    "Specifies a file to load for automatic configuration (default: '.sentry.yml')") do |file|
-    cli_config_file_name = file
-  end
-  parser.on(
-    "--install",
-    "Run 'shards install' once before running Sentry build and run commands") do
-    cli_config.install_shards = true
-  end
-  parser.on(
     "--no-color",
     "Removes colorization from output") do
     cli_config.colorize = false
@@ -72,13 +58,7 @@ OptionParser.parse! do |parser|
   end
 end
 
-config_yaml = ""
-if File.exists?(cli_config_file_name)
-  config_yaml = File.read(cli_config_file_name)
-end
-
-config = Sentry::Config.from_yaml(config_yaml)
-config.merge!(cli_config)
+config = cli_config
 
 if config.info
   if config.colorize?
@@ -88,7 +68,7 @@ if config.info
   end
 end
 
-if Sentry::Config.shard_name
+if Sentry::Config.name
   process_runner = Sentry::ProcessRunner.new(
     display_name: config.display_name!,
     build_command: config.build,
@@ -97,12 +77,11 @@ if Sentry::Config.shard_name
     run_args: config.run_args,
     should_build: config.should_build?,
     files: config.watch,
-    install_shards: config.install_shards?,
     colorize: config.colorize?
   )
 
   process_runner.run
 else
-  puts "🤖  Sentry error: 'name' not given and not found in shard.yml"
+  puts "🤖  Sentry error: 'name' not given"
   exit 1
 end
